cdc12c582b56710a122c16b34608c12a5e95d06d
kadaji777 <roinwani@gmail.com>
2024-04-25 12:26:34 +0300

    lab complete

---
+++ src/assets/app.jsx
@@ -0,0 +1,13 @@
+import {useState} from "react";
+import './App.css'
+
+// Add your code own within the return statement
+function App() {
+  return (
+<>
+<studentDetails/>
+</>
+  );
+}
+
+export default App;
---
+++ src/assets/student details.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+export function studentDetails (){
+    return <div>student</div>
+}
+export default studentDEtails
